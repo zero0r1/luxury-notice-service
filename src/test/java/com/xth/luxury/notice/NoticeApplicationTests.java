562735605d661baa2e51143dbd301f80d8f601ad
zero0r1 <leahdizonth@gmail.com>
2020-04-17 00:53:49 +0800

    add 增加redis服务

--- src/test/java/com/xth/luxury/notice/NoticeApplicationTests.java
+++ src/test/java/com/xth/luxury/notice/NoticeApplicationTests.java
@@ -1,10 +1,16 @@
 package com.xth.luxury.notice;
 
+import cn.hutool.core.lang.Console;
 import com.xth.luxury.notice.manager.LvNoticeManager;
+import com.xth.luxury.notice.utils.RedisUtil;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import javax.annotation.Resource;
+import javax.annotation.Resources;
 import java.net.InetSocketAddress;
 import java.util.List;
 
@@ -13,6 +19,8 @@ class NoticeApplicationTests {
 
     @Resource
     private LvNoticeManager lvNoticeManager;
+    @Resource
+    private RedisUtil redisUtil;
 
     @Test
     void contextLoads() {
@@ -20,4 +28,11 @@ class NoticeApplicationTests {
         int a = 1;
     }
 
+
+    @Test
+    void testRedis() {
+        boolean set = redisUtil.set("rest_redis", "test_value");
+        Object test_redis = redisUtil.get("rest_redis");
+        Console.log(test_redis);
+    }
 }
