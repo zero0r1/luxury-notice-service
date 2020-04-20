40233e676400114af309b3026f87e59fa9bd029a
zero0r1 <leahdizonth@gmail.com>
2020-04-20 21:41:12 +0800

    add 捕获发送邮件错误日志

--- src/main/java/com/xth/luxury/notice/manager/MailService.java
+++ src/main/java/com/xth/luxury/notice/manager/MailService.java
@@ -1,8 +1,10 @@
 package com.xth.luxury.notice.manager;
 
+import cn.hutool.log.StaticLog;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.MailException;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
@@ -14,7 +16,6 @@ import org.springframework.stereotype.Service;
  * @date 2020/4/13 3:59 下午
  */
 @Service
-@Slf4j
 public class MailService {
 
     @Value("${spring.mail.username}")
@@ -31,12 +32,16 @@ public class MailService {
      * @param content
      */
     public void sendSimpleTextMail(String to, String subject, String content) {
-        SimpleMailMessage message = new SimpleMailMessage();
-        message.setTo(to);
-        message.setSubject(subject);
-        message.setText(content);
-        message.setFrom(from);
-        mailSender.send(message);
-        log.info("【文本邮件】成功发送！to={}", to);
+        try {
+            SimpleMailMessage message = new SimpleMailMessage();
+            message.setTo(to);
+            message.setSubject(subject);
+            message.setText(content);
+            message.setFrom(from);
+            mailSender.send(message);
+            StaticLog.info("【文本邮件】成功发送！to={}", to);
+        } catch (Exception e) {
+            StaticLog.error(e.getCause(), "sendSimpleTextMail");
+        }
     }
 }
