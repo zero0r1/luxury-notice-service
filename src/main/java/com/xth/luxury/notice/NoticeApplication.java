a756e88d60f1fd9aba28d5a873befc12595be3bc
zero0r1 <leahdizonth@gmail.com>
2020-04-22 23:07:15 +0800

    获取结果设定超时时间

--- src/main/java/com/xth/luxury/notice/NoticeApplication.java
+++ src/main/java/com/xth/luxury/notice/NoticeApplication.java
@@ -1,15 +1,12 @@
 package com.xth.luxury.notice;
 
-import lombok.extern.log4j.Log4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
-import org.springframework.scheduling.annotation.EnableScheduling;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableAsync
-@EnableScheduling
 @EnableSwagger2
 public class NoticeApplication {
 
