88578f319f4419a94dd0412312d519885d0ea8a1
项天浩 <shawn_xiang@aliyun.com>
2020-05-07 17:38:45 +0800

    reformat code

--- src/main/java/com/xth/luxury/notice/manager/MailService.java
+++ src/main/java/com/xth/luxury/notice/manager/MailService.java
@@ -1,10 +1,8 @@
 package com.xth.luxury.notice.manager;
 
 import cn.hutool.log.StaticLog;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.mail.MailException;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
