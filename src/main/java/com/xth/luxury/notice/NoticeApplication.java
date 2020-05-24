f16e43ca288f2e92fe0e9fb8992d415f653321bb
zero0r1 <leahdizonth@gmail.com>
2020-05-24 15:43:36 +0800

    add 加入SpringUtil 扩展支持bean动态获取

--- src/main/java/com/xth/luxury/notice/NoticeApplication.java
+++ src/main/java/com/xth/luxury/notice/NoticeApplication.java
@@ -3,6 +3,8 @@ package com.xth.luxury.notice;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Import;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -12,6 +14,9 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableScheduling
 @EnableAsync
 @MapperScan("com.xth.luxury.notice.mapper")
+// 扫描cn.hutool.extra.spring包下所有类并注册之
+@ComponentScan(basePackages={"com.xth.luxury.notice","cn.hutool.extra.spring"})
+@Import(cn.hutool.extra.spring.SpringUtil.class)
 public class NoticeApplication {
 
     public static void main(String[] args) {
---
+++ src/test/java/com/xth/luxury/notice/services/ILvGoodsServiceTest.java
@@ -0,0 +1,16 @@
+package com.xth.luxury.notice.services;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ILvGoodsServiceTest {
+
+    @Test
+    void test() {
+
+    }
+}
